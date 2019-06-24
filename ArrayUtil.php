20129a14d977b75855224ec11cf2e7990f42585e
Anderson Martins <anderson.e.f.martins@hotmail.com>
2019-06-24 18:19:13 -0300

    Desativando um cast automatica do arquivo e do metodo

--- ArrayUtil.php
+++ ArrayUtil.php
@@ -1,12 +1,12 @@
-<?php
+<?php declare(strict_types=1); //Não faz o cast automatico
 class ArrayUtils{
 
     //não preciso instanciar para usar
     //& referencia, modifica direto na intergra e não copia, igual o sort
     public static function remover(string $elemento, array &$array){
 
-        //vai buscar o elemento x no array x
-        $posicao = array_search($elemento, $array);
+        //vai buscar o elemento x no array x, e compara o conteudo e o tipo
+        $posicao = array_search($elemento, $array,true);
         if(is_int($posicao)){
             var_dump($posicao);
             unset($array[$posicao]);
