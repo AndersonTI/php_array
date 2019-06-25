f54b867d7f0e0561cd75d9614b4567c2fe392627
Anderson Martins <anderson.e.f.martins@hotmail.com>
2019-06-25 16:14:35 -0300

    Declarando o array Associativo e iterando ele

--- src/Alura/arrayAssociadtivo.php
+++ src/Alura/arrayAssociadtivo.php
@@ -13,15 +13,29 @@ $saldo = [
 
 ];
 
-//Junto os dois array(colocando o final)
-//$novoArray = array_marge($corretistas,$saldo);
+$outroArrayAssociativo = [
+    "Anderson" => "320",
+    "Pamela" => "320",
+    "Anthony" => "430",
+
+];
+
+var_dump($outroArrayAssociativo);
+
+foreach ($outroArrayAssociativo as $key => $value) {
+    echo "O CPF do {$key} é {$value}".PHP_EOL;
+}
 
 //Array Associativo, quando usamos String como chave do array
 $novoArray = array_combine($corretistas,$saldo);
-//Buscando o valor do indice Anderson
-var_dump( $novoArray["Anderson"]);
+
+if(array_key_exists("Andersson",$novoArray)){
+    echo "O saldo é {$novoArray[Anderson]}".PHP_EOL;
+}else{
+    echo "Não existe essa pessoa";
+}
 
 //adicionando um indice no array
 $novoArray["Anthony"] = 5000;
 
-var_dump($novoArray);
+//var_dump($novoArray);
